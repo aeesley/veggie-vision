168723259be79d016596cda6448e41a91e1db7bd
Chris Kalama <chriskalama96@gmail.com>
2020-11-04 15:23:22 -0600

    Created a FindOne functionality to render the vegetable information into the index.handlebars.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -2,19 +2,27 @@ var db = require("../models");
 console.log(db.Vegetable);
 module.exports = function(app) {
         //GET route for getting all our vegetables
-    app.get("/api/vegetables", function(req,res){
+    app.get("/api/vegetables/:id", function(req,res){
+        var id = req.params.id;
+        console.log(id);
+        
+
+        db.Vegetable.findOne({ where: { id: id } }).then(function(dbVegetable){
+            console.log("HERE IS OUR VEGETABLES!!!!" ,dbVegetable);
+            res.json(dbVegetable);
+        })
         //findAll is a sequelize function to return all entries
-        db.Vegetable.findAll({}).then(function(dbVegetable){
-            console.log(dbVegetable);
-            //res.json(dbVegetable)
-            res.render('index', dbVegetable);
-        });
+        // db.Vegetable.findAll({}).then(function(dbVegetable){
+        //     console.log(dbVegetable);
+        //     //res.json(dbVegetable)
+        //     res.render('index', dbVegetable);
+        // });
     });
         //a POST route, how to save a vegetable
-    app.post("/api/vegetables", function(req,res){
-        db.Vegetable.create({
-        }).then(function(dbVegetable){
-            res.json(dbVegetable);
-        });
-    });
+    // app.post("/api/vegetables", function(req,res){
+    //     db.Vegetable.create({
+    //     }).then(function(dbVegetable){
+    //         res.json(dbVegetable);
+    //     });
+    // });
 }
