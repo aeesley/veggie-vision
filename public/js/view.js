144abd8b137d23a02761491fc1894feaf805d923
Chris Kalama <chriskalama96@gmail.com>
2020-11-04 20:36:02 -0600

    nested localStorage.setItem into ajax GET call

--- public/js/view.js
+++ public/js/view.js
@@ -48,9 +48,27 @@ $(document).ready(function(){
                 console.log(res.vegetable_name);
                 $(".modal-body").html(`Here is a ${res.vegetable_name}, you can eat this many ${res.serving} and has this many ${res.calories} calories. `);
                 $(".modal-title").html(`${res.vegetable_name}`);
-            }
+
+                $(document).ready(function(){
+                    $("#save-btn").on("click", function(){
+                        //console.log("CLICKKKKK")
+        
+                    var vegName = res.vegetable_name;
+                    
+                    localStorage.setItem("vegetableName", vegName);
+
+                    //JSON.parse(localStorage.getItem("user")); --> This should be used to parse the name of the vegetable onto the screen where we want it. 
+                    
+                })
+            })
+            
+        }
+            
+            
         })
-    })
+        
+})
+
 })
 
 
@@ -58,42 +76,33 @@ $(document).ready(function(){
 
 
 //Setting up our local Storage
-$(document).ready(function(){
-    $("#save-btn").on("click", function(){
-        console.log("CLICKKKKK")
+// $(document).ready(function(){
+//     $("#save-btn").on("click", function(){
+//         console.log("CLICKKKKK")
 
-        var vegName = 
+//         var vegName = 
          
-        //window.localStorage = Gives yu access to a property, can just omit the 'window.'
-        //localStorage gives you a local object. 
-        //console.log(localStorage);
+//         //window.localStorage = Gives yu access to a property, can just omit the 'window.'
+//         //localStorage gives you a local object. 
+//         //console.log(localStorage);
 
-        //setItem takes two elements, a key and a value! The value is also automatically converted to a string if its an integer
+//         //setItem takes two elements, a key and a value! The value is also automatically converted to a string if its an integer
 
-            //For us, we need that value to produce what is being displayed on the screen. Or just the Vegetable?
-        localStorage.setItem("Vegetable", "vegetableName")
-        //If you delete the above ^ you will still see it in the localStorage even if you refresh the server. Data does not expire.
+//             //For us, we need that value to produce what is being displayed on the screen. Or just the Vegetable?
+//         localStorage.setItem("Vegetable", vegName);
+//         //If you delete the above ^ you will still see it in the localStorage even if you refresh the server. Data does not expire.
 
-        //If we need to remove an item: localStorage.removeItem("key");
+//         //If we need to remove an item: localStorage.removeItem("key");
             
-        //Testing to see if we can grab the above key with a getItem.
-        //console.log(localStorage.getItem("Vegetable"));
+//         //Testing to see if we can grab the above key with a getItem.
+//         //console.log(localStorage.getItem("Vegetable"));
 
-        //Testing keys in our localStorage, this will grab the first key in localStorage, and display it on console. 
-        //console.log(localStorage.key(0));
+//         //Testing keys in our localStorage, this will grab the first key in localStorage, and display it on console. 
+//         //console.log(localStorage.key(0));
          
-    })
-})
-
-
-// $("#save-btn").on("click", function(){
-//     // var key = $(this).parent().attr("id")
-//     console.log("CLICKKKKK")
-//     // var key = window.localStorage.key(keyOne);
-    
-//     // localStorage.setItem('keyOne', btn);
-
+//     })
 // })
+
 
 //This is the npm package Local Storage
     // if (typeof localStorage === "undefined" || localStorage === null) {
